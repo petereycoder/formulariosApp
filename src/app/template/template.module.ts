02868f116002abf25e491742e41f9b56e1d91cf0
petereycoder <fp.pipe.reyes@gmail.com>
2022-02-16 19:22:30 -0600

    Basicos template

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    TemplateRoutingModule
+    TemplateRoutingModule,
+    FormsModule
   ]
 })
 export class TemplateModule { }
