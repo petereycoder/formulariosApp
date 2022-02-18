103dc8dd93308333b205d58a15aa1248782e5b6c
petereycoder <fp.pipe.reyes@gmail.com>
2022-02-17 19:43:48 -0600

    Template segunda parte

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -11,6 +11,12 @@ export class BasicosComponent implements OnInit {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'Xbox 360',
+    precio: 10,
+    existencias: 10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -21,9 +27,28 @@ export class BasicosComponent implements OnInit {
            this.miFormulario?.controls['producto']?.touched;
   }
 
+  precioValido(): boolean{
+    return (this.miFormulario?.controls['precio']?.touched &&
+           this.miFormulario?.controls['precio']?.invalid)?
+           false:true;
+
+  }
+
+  existenciaValido(): boolean{
+    return (this.miFormulario?.controls['existencias']?.touched &&
+           this.miFormulario?.controls['existencias']?.invalid)?
+           false:true;
+
+  }
+
   //guardar(miFormulario: NgForm){
   guardar(){
-    console.log(this.miFormulario);
+    console.log('Posteo correcto');
+    this.miFormulario.resetForm({
+      producto: 'Algo',
+      precio: 0,
+      existencias: 0
+    });
   }
 
 }
--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +16,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Felipe',
+    favoritos: [
+      { id: 1, nombre: 'Metal slug'},
+      { id: 2, nombre: 'Plantas vs zombies'},
+    ]
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( { ...nuevoFavorito } );
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  guardar(){
+    console.log('Formulario posteado');
   }
 
 }
