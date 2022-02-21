55adcd3552584a060443e9abeab3ab64df5fe5d2
petereycoder <fp.pipe.reyes@gmail.com>
2022-02-21 12:08:38 -0600

    Formularios reactivos segunda parte

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -15,12 +15,37 @@ export class DinamicosComponent implements OnInit {
   }
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [ , [Validators.required, Validators.minLength(3) ] ]
+    nombre: [ '' , [Validators.required, Validators.minLength(3) ] ],
+    favoritos: this.fb.array( [
+      [ 'Metal Slug', Validators.required ],
+      [ 'The king of fither', Validators.required ],
+    ], Validators.required )
   })
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArray(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   campoNoEsValido( campo: string ){
     return this.miFormulario.controls[campo].errors &&
            this.miFormulario.controls[campo].touched;
+  }
+
+  agregarFavorito(){
+
+    if( this.nuevoFavorito.invalid ){ return; }
+
+    //this.favoritosArray.push( new FormControl( this.nuevoFavorito.value, Validators.required ) );
+    this.favoritosArray.push( this.fb.control( this.nuevoFavorito.value, Validators.required) );
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrar(i: number){
+    
+    this.favoritosArray.removeAt(i);
   }
 
   guardar(){
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    condiciones: [ false, Validators.requiredTrue],
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+      this.miFormulario.reset( {
+         ...this.persona,
+         condiciones: false
+       });
+
+       this.miFormulario.valueChanges.subscribe( ({ condiciones, ...rest}) => {
+        this.persona = rest;
+      })
   }
+
+  guardar(){
+
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+
+  }
+
 
 }
